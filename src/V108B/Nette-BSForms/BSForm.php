d1b98c42f53ebd3561aa8dba72c3bbbe94861cb1
Vaclav Juchelka <vjuchelka@gmail.com>
2015-03-05 17:49:23 +0100

    cleanup

--- src/V108B/Nette-BSForms/BSForm.php
+++ src/V108B/Nette-BSForms/BSForm.php
@@ -3,53 +3,50 @@
 namespace V108B\NetteBSForms;
 
 use Nette,
-    Nette\Application\UI,
-	Nette\Utils\Html,
-	Tracy\Debugger;
+	Nette\Application\UI;
 
 class BSForm extends UI\Control {
 
-	public $templateName;
 	private $form;
 	private $formName;
+	private $style = null;
+
+	const STYLE_PANEL = 'panel';
+	const STYLE_CLEAN = 'clean';
 
 	public function __construct($formName = '') {
-		//parent::__construct();
 		$this->formName = $formName;
 	}
 
-	private function getForm($templateName = 'form.latte')
-	{
-		if (!isset($this->form)) {
-			$this->form = new UI\Form;
-			// mohu použít $this->database
-
-			if ($templateName != '') {
-				$this->templateName = $templateName;
-			}
-		}
-
-		return $this->form;
-	}
-
 	public function getFormClean() {
-		return $this->getForm('BSFormClean.latte');
+		$this->style = static::STYLE_PANEL;
+		return $this;
 	}
 
 	public function getFormPanel() {
-		return $this->getForm('BSFormPanel.latte');
+		$this->style = static::STYLE_PANEL;
+		return $this;
 	}
 
-
-	public function render() {
-
-		$template = $this->getTemplate();
-		$template->setFile(__DIR__ . '/' . $this->templateName);
-		Debugger::barDump($this->templateName);
-		$template->controlClass = 'col-md-3';
-		$template->form = $this->getForm();
-		$template->formName = $this->formName;
-		$template->render();
+	public function createComponentForm()
+	{
+		return new UI\Form();
 	}
 
+	public function getTemplateFilename($style)
+	{
+		switch($style) {
+			case static::STYLE_PANEL: return 'BSFormPanel.latte';
+			case static::STYLE_CLEAN: return 'BSFormClean.latte';
+			default: return 'form.latte';
+		}
+	}
+
+	public function render($style = null) {
+		$this->template->setFile(__DIR__ . '/' . $this->getTemplateFilename($style ?: $this->style));
+		$this->template->controlClass = 'col-md-3';
+		$this->template->form = $this['form'];
+		$this->template->formName = $this->formName;
+		$this->template->render();
+	}
 }
